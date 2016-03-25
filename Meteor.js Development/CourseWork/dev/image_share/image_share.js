85da4042858003e16e55346cdd021f446891766b
highslater <highslater@hotmail.com>
2016-03-24 22:02:42 -0400

    commit

--- Meteor.js Development/CourseWork/dev/image_share/image_share.js
+++ Meteor.js Development/CourseWork/dev/image_share/image_share.js
@@ -1,23 +1,23 @@
 
 Images = new Mongo.Collection('images');
-/*console.log(Images.find().count());*/
 
 if (Meteor.isClient) {
 
   Template.images.helpers({images: Images.find()});
 
   Template.images.events({
+
     'click .js-image': function(event){
      $(event.target).css("width", "50px");
-    },
+    }, // end of click .js-image
+
     'click .js-del-image': function(event){
       var image_id = this._id;
-      /*console.log(image_id);*/
       $('#' + image_id).hide('slow', function(){
         Images.remove({_id: image_id});
-      })
-      
-    }
+      }) // end of function
+    }, // end of click .js-del-image
 
-  });
-}
+  }); // end of Template.images.events
+
+} // end of if (Meteor.isClient)
