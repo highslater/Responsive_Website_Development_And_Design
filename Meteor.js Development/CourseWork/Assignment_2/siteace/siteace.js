a113bb3c5f44e2d62c321471c1085bdfcf8a97dd
highslater <highslater@hotmail.com>
2016-03-26 12:26:19 -0400

    commit

--- Meteor.js Development/CourseWork/Assignment_2/siteace/siteace.js
+++ Meteor.js Development/CourseWork/Assignment_2/siteace/siteace.js
@@ -23,7 +23,7 @@ if (Meteor.isClient) {
 			// example of how you can access the id for the website in the database
 			// (this is the data context for the template)
 			var website_id = this._id;
-			console.log("Up voting website with id "+website_id);
+			console.log("< from client > siteace.js Says: Up voting website with id "+website_id);
 			// put the code in here to add a vote to a website!
 
 			return false;// prevent the button from reloading the page
@@ -33,7 +33,7 @@ if (Meteor.isClient) {
 			// example of how you can access the id for the website in the database
 			// (this is the data context for the template)
 			var website_id = this._id;
-			console.log("Down voting website with id "+website_id);
+			console.log("< from client > siteace.js Says: Down voting website with id "+website_id);
 
 			// put the code in here to remove a vote from a website!
 
@@ -49,7 +49,7 @@ if (Meteor.isClient) {
 
 			// here is an example of how to get the url out of the form:
 			var url = event.target.url.value;
-			console.log("The url they entered is: "+url);
+			console.log("< from client > siteace.js Says: The url they entered is: "+url);
 			
 			//  put your website saving code in here!	
 
@@ -65,7 +65,7 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
     if (!Websites.findOne()){
-    	console.log("No websites yet. Creating starter data.");
+    	console.log("< from server > siteace.js Says: No websites yet. Creating starter data.");
     	  Websites.insert({
     		title:"Goldsmiths Computing Department", 
     		url:"http://www.gold.ac.uk/computing/", 
